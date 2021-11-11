fea9df6862472633439c4c8d8bc1578abbe6bf5a
nadrod <60800898+nadrod@users.noreply.github.com>
2021-11-11 14:04:05 -0600

    Add files via upload

---
+++ Usb_File_Copy.ps1
@@ -0,0 +1,16 @@
+﻿function Get-Usb{
+    $drives = [System.IO.DriveInfo]::GetDrives()
+    $r = $drives | Where-Object { $_.DriveType -eq 'Removable' -and $_.IsReady }
+    if ($r) {
+        return @($r)[-1]
+    }
+    throw "No removable drives found."
+}
+
+$usb = Get-Usb
+$usb.RootDirectory.Name
+
+$ruta = Get-Location
+
+New-Item -Path -Path $ruta\Usb_Copy\ -ItemType directory -Force 
+Copy-Item -Path $usb.RootDirectory.Name -Destination $ruta\Usb_Copy\ -Recurse -Force -Passthru
